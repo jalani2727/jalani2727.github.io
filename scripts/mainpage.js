270aa6a5d7d75d368fedca94dbabde6a1eabec9c
Jalani Paul <Lala@Blessing.local>
2018-07-12 17:42:42 -0400

    Finalized MainPage Layout. Need to Figure out how to change all content when links on the left are clicked.

---
+++ scripts/mainpage.js
@@ -0,0 +1,11 @@
+var array = [1,2,3,4];
+
+function printNumbers() {
+    var count = 0;
+    for(var i=0; i<=array.length; i++){
+        console.log(array[i]);
+        count= count+1;
+    };
+};
+
+printNumbers();
