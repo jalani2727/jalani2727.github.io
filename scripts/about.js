3cfe646b8568c4947020116e0362c9af32b4d684
Jalani Paul <jalani2727@gmail.com>
2018-07-13 14:48:40 -0400

    removed jquery dynamic page changing for simpler multiple-webpage structure. Js files and script tags still present

--- scripts/about.js
+++ scripts/about.js
@@ -1,4 +1,17 @@
 $(document).ready(function(){
+    // $("#folioResume").on("click", function(event){
+    //     event.preventDefault();
+    //     $("#content").hide("fast", function(){
+    //         console.log("content hidden");
+    //         $("#resumecontent").load("resume.html", function(){
+    //             console.log("content loaded");
+    //             $("#content").show("fast");
+    //         });
+    //     });
+    // });
+
+
+
     $(".tldrButton").on("click", function(){
         $("p").addClass("tldr");
         $("section").html("<h2>tl;dr</h2><ul> <li>Graduated from Emory in 2017</li> <li>Worked a non-tech gig immediately afterwards for 3 months</li><li>Quickly became unfulfilled and made a career change</li><li>Re-ignited an ever-present passion for technology</li><li>Stuided at the DigitalCrafts coding Bootcamp to explore that passion</li><li>My fire of passion has become perpetually stoked and I am ready to continue developing my skills as a developer with a reputable organization!</li>")
