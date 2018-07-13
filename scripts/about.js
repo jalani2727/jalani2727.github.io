95e4d6e5fb32649ec1d5e554a20e83b28fd84853
Jalani Paul <Lala@Blessing.local>
2018-07-13 10:00:29 -0400

    added tldr button with jquery

--- scripts/about.js
+++ scripts/about.js
@@ -1,11 +1,6 @@
-var array = [1,2,3,4];
-
-function printNumbers() {
-    var count = 0;
-    for(var i=0; i<=array.length; i++){
-        console.log(array[i]);
-        count= count+1;
-    };
-};
-
-printNumbers();
+$(document).ready(function(){
+    $(".tldrButton").on("click", function(){
+        $("p").addClass("tldr");
+        $("section").html("<h2>tl;dr</h2><ul> <li>Graduated from Emory in 2017</li> <li>Worked a non-tech gig immediately afterwards for 3 months</li><li>Quickly became unfulfilled and made a career change</li><li>Re-ignited an ever-present passion for technology</li><li>Stuided at the DigitalCrafts coding Bootcamp to explore that passion</li><li>My fire of passion has become perpetually stoked and I am ready to continue developing my skills as a developer with a reputable organization!</li>")
+    });
+});
