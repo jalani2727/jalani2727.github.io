b51f4f6916677058e4d1236bfe951012c5a0ee2b
Jalani Paul <Lala@Blessing.local>
2018-07-14 08:32:57 -0400

    everything is working ahhhh

--- scripts/about.js
+++ scripts/about.js
@@ -1,19 +1,11 @@
-$(document).ready(function(){
-    // $("#folioResume").on("click", function(event){
-    //     event.preventDefault();
-    //     $("#content").hide("fast", function(){
-    //         console.log("content hidden");
-    //         $("#resumecontent").load("resume.html", function(){
-    //             console.log("content loaded");
-    //             $("#content").show("fast");
-    //         });
-    //     });
-    // });
-
-
-
-    $(".tldrButton").on("click", function(){
-        $("p").addClass("tldr");
-        $("section").html("<h2>tl;dr</h2><ul> <li>Graduated from Emory in 2017</li> <li>Worked a non-tech gig immediately afterwards for 3 months</li><li>Quickly became unfulfilled and made a career change</li><li>Re-ignited an ever-present passion for technology</li><li>Stuided at the DigitalCrafts coding Bootcamp to explore that passion</li><li>My fire of passion has become perpetually stoked and I am ready to continue developing my skills as a developer with a reputable organization!</li>")
+$(function(){ // <-- shorthand for $(document).ready
+     $(".tldrButton").on("click", function(){
+            $("p").hide();
+            $("section > h2").hide();
+            $("section > h3").hide();
+            $(".tldrbutton").hide();
+            $(".dynamiccontent").html("<h2>TL;DR</h2> <form method='get' action='about.html'><button type='submit'>Reload</button></form> <br> <br> <ul> <li>Graduated from Emory in 2017</li> <li>Worked a non-tech gig immediately afterwards for 3 months</li><li>Quickly became unfulfilled and made a career change</li><li>Re-ignited an ever-present passion for technology</li><li>Stuided at the DigitalCrafts coding Bootcamp to explore that passion</li><li>My fire of passion has become perpetually stoked and I am ready to continue developing my skills as a developer with a reputable organization!</li>");
+    
     });
+    
 });
