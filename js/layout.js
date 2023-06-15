087208a0ee125a0252cd37769f44e4d3c9ae7f94
jalani2727 <jalani2727@gmail.com>
2023-06-15 17:07:13 -0400

    Add website experience and carousels for video content - TODO: Imbed Google Drive iframes for relvant videos in website section

--- js/layout.js
+++ js/layout.js
@@ -1,3 +1,6 @@
+// require('bootstrap'); - only suitable in a node js environment. I am currently viewing this page as html in the browser.
+
+
 var Layout = function () {
     
     // detect mobile device
@@ -54,67 +57,67 @@ var Layout = function () {
         });
     }
 
-    // handle carousel
-    var handleCarousel = function() {
-        var $item = $('.carousel .item'); 
-        var $wHeight = $(window).height();
-        $item.eq(0).addClass('active');
-        $item.height($wHeight); 
-        $item.addClass('full-screen');
+    // // handle carousel
+    // var handleCarousel = function() {
+    //     var $item = $('.carousel .item'); 
+    //     var $wHeight = $(window).height();
+    //     $item.eq(0).addClass('active');
+    //     $item.height($wHeight); 
+    //     $item.addClass('full-screen');
 
-        $('.carousel img').each(function() {
-            var $src = $(this).attr('src');
-            var $color = $(this).attr('data-color');
-            $(this).parent().css({
-                'background-image' : 'url(' + $src + ')',
-                'background-color' : $color
-            });
-            $(this).remove();
-        });
+    //     $('.carousel img').each(function() {
+    //         var $src = $(this).attr('src');
+    //         var $color = $(this).attr('data-color');
+    //         $(this).parent().css({
+    //             'background-image' : 'url(' + $src + ')',
+    //             'background-color' : $color
+    //         });
+    //         $(this).remove();
+    //     });
 
-        $(window).on('resize', function (){
-            $wHeight = $(window).height();
-            $item.height($wHeight);
-        });
-    }
+    //     $(window).on('resize', function (){
+    //         $wHeight = $(window).height();
+    //         $item.height($wHeight);
+    //     });
+    // }
 
     // handle group element heights
-    var handleHeight = function() {
-       $('[data-auto-height]').each(function() {
-            var parent = $(this);
-            var items = $('[data-height]', parent);
-            var height = 0;
-            var mode = parent.attr('data-mode');
-            var offset = parseInt(parent.attr('data-offset') ? parent.attr('data-offset') : 0);
+    // var handleHeight = function() {
+    //    $('[data-auto-height]').each(function() {
+    //         var parent = $(this);
+    //         var items = $('[data-height]', parent);
+    //         var height = 0;
+    //         var mode = parent.attr('data-mode');
+    //         var offset = parseInt(parent.attr('data-offset') ? parent.attr('data-offset') : 0);
 
-            items.each(function() {
-                if ($(this).attr('data-height') == "height") {
-                    $(this).css('height', '');
-                } else {
-                    $(this).css('min-height', '');
-                }
+    //         items.each(function() {
+    //             if ($(this).attr('data-height') == "height") {
+    //                 $(this).css('height', '');
+    //             } else {
+    //                 $(this).css('min-height', '');
+    //             }
 
-                var height_ = (mode == 'base-height' ? $(this).outerHeight() : $(this).outerHeight(true));
-                if (height_ > height) {
-                    height = height_;
-                }
-            });
+    //             var height_ = (mode == 'base-height' ? $(this).outerHeight() : $(this).outerHeight(true));
+    //             if (height_ > height) {
+    //                 height = height_;
+    //             }
+    //         });
 
-            height = height + offset;
+    //         height = height + offset;
 
-            items.each(function() {
-                if ($(this).attr('data-height') == "height") {
-                    $(this).css('height', height);
-                } else {
-                    $(this).css('min-height', height);
-                }
-            });
+    //         items.each(function() {
+    //             if ($(this).attr('data-height') == "height") {
+    //                 $(this).css('height', height);
+    //             } else {
+    //                 $(this).css('min-height', height);
+    //             }
+    //         });
 
-            if(parent.attr('data-related')) {
-                $(parent.attr('data-related')).css('height', parent.height());
-            }
-       });
-    }
+    //         if(parent.attr('data-related')) {
+    //             $(parent.attr('data-related')).css('height', parent.height());
+    //         }
+    //    });
+    // }
 
     return {
         init: function () {
